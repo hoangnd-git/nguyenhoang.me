a8ba62afb02be721693876a4d5b01848331460f2
jacky nguyen <nguyenhoang96.arsenal@gmail.com>
2023-04-04 15:02:04 +0700

    release

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,20 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.scss';
+import type { AppProps } from 'next/app';
+import Link from 'next/link';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <div className="header">
+        <div className="header-center">
+          <Link href="/" className="pointer">
+            <span className="branch">nguyenhoang.me</span>
+          </Link>
+          <div className="text-animate"></div>
+        </div>
+      </div>
+      <Component {...pageProps} />
+      <footer className="footer"></footer>
+    </>
+  );
 }
